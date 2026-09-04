37de17b2dd84d048e1481c32ffe8398704195fae
Abhishek <vikashabhishek123@gmail.com>
2026-09-04 10:04:59 +0530

    new commit

--- day1.c
+++ day1.c
@@ -1,9 +1,31 @@
+//today we will discuss about switch statement it is also like if, else-if
+//here i am going to create a program which takes alphabetical input from user and tells them that given input is vowel or not;
+
 #include <stdio.h>
 int main(){
-    char ch='a';
-    if(ch=='a'||ch=='e'||ch=='i'){
-        printf("this is vowel");
-    }else{
-        printf("constant");
-    };
+    char ch;
+    printf("please enter any alphabet");
+    scanf(" %c",&ch);
+
+    switch(ch){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("this is vowel");
+            break;
+
+        default:
+            printf("this is consonant");
+
+
+    }
+    return 0;
+
 }
