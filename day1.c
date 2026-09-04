5186335350606d0279e59ecb35a0233dee47e7c9
Abhishek <vikashabhishek123@gmail.com>
2026-09-04 10:33:30 +0530

    new commit added

--- day1.c
+++ day1.c
@@ -29,3 +29,35 @@ int main(){
     return 0;
 
 }
+
+
+//Question 1: Check whether a number is positive, negative, or zero using switch
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    switch (num > 0) {
+        case 1:
+            printf("Positive number");
+            break;
+
+        case 0:
+            switch (num < 0) {
+                case 1:
+                    printf("Negative number");
+                    break;
+
+                case 0:
+                    printf("Zero");
+                    break;
+            }
+            break;
+    }
+
+    return 0;
+}
