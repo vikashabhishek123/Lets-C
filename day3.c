91046d99ad7d0a0a50b3bfe29dc8908afe19f49c
Abhishek <vikashabhishek123@gmail.com>
2026-09-06 08:59:04 +0530

    new commit on loop

--- day3.c
+++ day3.c
@@ -11,3 +11,19 @@ int main(){
     }
     return 0;
 }
+
+
+//Question2 Take input from user and add all numbers
+#include <stdio.h>
+int main(){
+    int a;
+    int sum=0;
+    printf("Please enter the number");
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        sum=sum+i;
+
+    }
+    printf("The sum of total number is %d",sum);
+    return 0;
+}
