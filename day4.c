1bcec6e8f4d7db0a5c3d24444d2905a7127b2bac
Abhishek <vikashabhishek123@gmail.com>
2026-09-07 21:12:29 +0530

    MM MGM cutie

--- day4.c
+++ day4.c
@@ -8,3 +8,48 @@ int main(){
     area=PI*(radius*radius);
     printf("the area is %f",area);
 }
+
+//Question2 Here we have to take the age of user and if age is greater than 18 then print you can drive if he is less than 18 then print you cannot drive
+#include <stdio.h>
+
+int main() {
+    int age;
+    char Name[10];
+
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+
+    printf("Please enter your name: ");
+    scanf("%9s", Name);
+
+    if (age >= 18) {
+        printf("You can drive the vehicle");
+    } else {
+        printf("You cannot drive");
+    }
+
+    return 0;
+}
+
+
+//Question3 Same question hai but isme name ke saath ayega like if maine name enter kiya Abhishek so ayega abhishek you can drive
+#include <stdio.h>
+
+int main() {
+    int age;
+    char Name[20];
+
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+
+    printf("Please enter your name: ");
+    scanf("%19s", Name);
+
+    if (age >= 18) {
+        printf("%s, you can drive the vehicle.", Name);
+    } else {
+        printf("%s, you cannot drive the vehicle.", Name);
+    }
+
+    return 0;
+}
