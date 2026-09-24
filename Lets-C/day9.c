f5eaa4d8f13ce44e6806c7243137ef3b91dfa561
Abhishek <vikashabhishek123@gmail.com>
2026-09-24 07:09:57 +0530

    looping on array

--- Lets-C/day9.c
+++ Lets-C/day9.c
@@ -24,3 +24,33 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+  // An array storing different ages
+  int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+ 
+  float avg, sum = 0;
+  int i;
+  
+  // Get the length of the array
+  int length = sizeof(ages) / sizeof(ages[0]);
+    
+  // Loop through the elements of the array and accumulate the sum
+  for (i = 0; i < length; i++) {
+    sum += ages[i];
+  }
+  
+  // Calculate the average by dividing the sum by the length
+  avg = sum / length;
+  
+  // Print the average
+  printf("The average age is: %.2f", avg);
+  
+  return 0;
+}
